8281f9a6a28504bdde5dc5cd161efc06bdfab376
Anubhav <gupta.anubhav25@gmail.com>
2019-05-02 23:48:35 +0530

    Updates

---
+++ precache-manifest.09ee204ef9d410dc30cc1014c92cd076.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4498ea8349e9e1ed11bda02df9939667",
+    "url": "/Socializer_React/index.html"
+  },
+  {
+    "revision": "cb37ffe442e8820642d2",
+    "url": "/Socializer_React/static/css/2.951b227e.chunk.css"
+  },
+  {
+    "revision": "089e92a12c3ac5855a80",
+    "url": "/Socializer_React/static/css/main.a1c5dc14.chunk.css"
+  },
+  {
+    "revision": "cb37ffe442e8820642d2",
+    "url": "/Socializer_React/static/js/2.6d5379d4.chunk.js"
+  },
+  {
+    "revision": "089e92a12c3ac5855a80",
+    "url": "/Socializer_React/static/js/main.9cd749af.chunk.js"
+  },
+  {
+    "revision": "c4cab018aa8675ce2c02",
+    "url": "/Socializer_React/static/js/runtime~main.727962d2.js"
+  },
+  {
+    "revision": "cc21d65a246542d885a604119623d9b6",
+    "url": "/Socializer_React/static/media/downloads-bg.cc21d65a.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Socializer_React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Socializer_React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Socializer_React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Socializer_React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Socializer_React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "23c893eaa8caee50a6d736679fdc4374",
+    "url": "/Socializer_React/static/media/intro-bg.23c893ea.jpg"
+  }
+]);
